5bd63888189d176ac7883d29c3b7e4f2a0f6370c
DWhiteheadCode <95430831+DWhiteheadCode@users.noreply.github.com>
2024-04-09 15:49:08 +0800

    Create blink and pulse bomb actions, and re-parent correpsonding blueprints

---
+++ Source/Tracer_Abilities/Public/TAction_PulseBomb.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TAction.h"
+#include "TAction_PulseBomb.generated.h"
+/**
+ * 
+ */
+UCLASS()
+class TRACER_ABILITIES_API UTAction_PulseBomb : public UTAction
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Tracer_Abilities/Public/TAction_Recall.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TAction.h"
+#include "TAction_Recall.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRACER_ABILITIES_API UTAction_Recall : public UTAction
+{
+	GENERATED_BODY()
+	
+};

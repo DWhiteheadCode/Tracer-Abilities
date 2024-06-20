f70f443e80ff08e655dd2464fe44865cfc56303f
DWhiteheadCode <95430831+DWhiteheadCode@users.noreply.github.com>
2024-06-20 11:54:28 +0800

    Remove accidental manual re-implementation of FHitResult.Location

--- Source/Tracer_Abilities/Private/TAction_Blink.cpp
+++ Source/Tracer_Abilities/Private/TAction_Blink.cpp
@@ -105,12 +105,8 @@ FVector UTAction_Blink::GetTeleportDestination(ACharacter* const CharacterToTele
 				DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 2, 8, FColor::Red, false, 5.0f);
 			}			
 
-			FVector ReversedSweepDirection = StartLocation - EndLocation;
-			ReversedSweepDirection.Normalize();
-
 			// Set the location such that the edge of the character's capsule will *just* touch the impact point
-			EndLocation = HitResult.ImpactPoint + (ReversedSweepDirection * CharacterCapsule->GetScaledCapsuleRadius());
-			EndLocation.Z = StartLocation.Z; // Ignore vertical change based on where the collision occurred
+			EndLocation = HitResult.Location;
 
 			DebugCapsuleColor = FColor::Red;
 		}
